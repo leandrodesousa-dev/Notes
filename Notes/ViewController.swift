8cf778bdcd7834c4fab9862ed6831763a6b6e796
Leandro <leandrooodesousa@gmail.com>
2019-09-15 16:13:57 -0400

    Initial Commit

---
+++ Notes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Notes
+//
+//  Created by Leandro de Sousa Silva on 15/09/19.
+//  Copyright © 2019 AcademyMistic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
